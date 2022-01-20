eefa42040a55eabb94b38681e84ff297112b5aab
NoahNim <nmedoff123@gmail.com>
2022-01-19 18:04:17 -0800

    wrap in domcontentloaded and make event handler fr submit

---
+++ timecountdown/script.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded", (event) => {
+    let selectedTime = document.getElementById("time");
+    let timeForm = document.getElementById("time-form");
+    let countdownEle = document.getElementById("countdown");
+    let theCount = '';
+
+    timeForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        countdownEle.innerHTML = selectedTime;
+    });
+});
